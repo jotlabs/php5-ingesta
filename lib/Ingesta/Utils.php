56a5e1574607217251cd9e8a80891f34ef10fece
Mike Davies <isofarro2@gmail.com>
2012-09-25 21:18:11 +0100

    clean up autoloader classes

--- lib/Ingesta/Utils.php
+++ lib/Ingesta/Utils.php
@@ -2,7 +2,7 @@
 namespace Ingesta;
 
 class Utils {
-    public static $libDir;
+    private static $__libDir;
 
     /******************************************************************
     * PSR-0 Autoloader
@@ -10,7 +10,7 @@ class Utils {
     * Do not use if you are using Composer to autoload dependencies.
     ******************************************************************/
     public static function autoload($className) {
-        $fileName  = self::$libDir;
+        $fileName  = self::$__libDir;
         $classPath = str_replace('\\', '/', ltrim($className, '\\'));
         $classPath = str_replace('_',  '/', $classPath);
         $fileName .= $classPath . '.php';
@@ -19,40 +19,10 @@ class Utils {
             require $fileName;
         }
     }
-    // Previous autoload version
-    public static function autoloadXX($className) {
-        $thisClass = str_replace(__NAMESPACE__ . '\\', '', __CLASS__);
-        $baseDir   = __DIR__;
-
-        $classLen  = strlen($thisClass);
-        if (substr($baseDir, -$classLen) === $thisClass) {
-            $baseDir = substr($baseDir, 0, -$classLen);
-        }
-
-        $className = ltrim($className, '\\');
-        $fileName  = $baseDir;
-        $namespace = '';
-        
-        $lastNsPos = strripos($className, '\\');
-        if ($lastNsPos) {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace)
-                    . DIRECTORY_SEPARATOR;
-        }
-
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className)
-                . '.php';
-
-        if (file_exists($fileName)) {
-            require $fileName;
-        }
-    }
-
 
     public static function registerAutoloader() {
         $classFile = str_replace('\\', '/', __CLASS__ . '.php');
-        self::$libDir = str_replace($classFile, '', __FILE__);
+        self::$__libDir = str_replace($classFile, '', __FILE__);
         spl_autoload_register(__NAMESPACE__ . "\\Utils::autoload");
     }
 
