a6530edac9f9d7d6ff425a699f039ab6d3ee58be
Mike Davies <isofarro2@gmail.com>
2014-03-22 12:58:05 +0000

    Add test to check Ingesta exists

--- tests/Ingesta/IngestaTest.php
+++ tests/Ingesta/IngestaTest.php
@@ -6,6 +6,11 @@ use PHPUnit_Framework_TestCase;
 class IngestaTest extends PHPUnit_Framework_TestCase
 {
 
+	public function testClassExists()
+	{
+		$this->assertTrue(class_exists('Ingesta\Ingesta'));
+	}
+
     public function testHasVersion()
     {
         $this->assertTrue(is_string(Ingesta::VERSION));
