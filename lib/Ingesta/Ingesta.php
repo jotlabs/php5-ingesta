6116498b55c2d588fbc80a8632a5cc4cfb813263
Mike Davies <mike@jotlabs.com>
2014-03-22 13:48:38 +0000

    Switch Ingesta to static invoker, with command line args.

--- lib/Ingesta/Ingesta.php
+++ lib/Ingesta/Ingesta.php
@@ -5,8 +5,31 @@ class Ingesta
 {
     const VERSION = '0.0.1';
 
-    public function run($argv)
+
+    public static function run()
+    {
+        $self = new Ingesta();
+        $cmdArgs = $self->getCmdArgs();
+        $self->execute($cmdArgs);
+    }
+
+
+    public function execute($args)
     {
         echo 'Ingesta ', self::VERSION, ": Running...\n";
+        print_r($args);
+    }
+
+
+    protected function getCmdArgs()
+    {
+        $args = getopt(
+            '',
+            array(
+                'help'
+            )
+        );
+
+        return $args;
     }
 }
