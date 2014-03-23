5096a5ff6a691e864dafce8db23574bc685c726e
Mike Davies <isofarro2@gmail.com>
2014-03-23 11:30:33 +0000

    remove commented-out code

--- share/tests/zend_xmlrpc_client.php
+++ share/tests/zend_xmlrpc_client.php
@@ -3,7 +3,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Zend\XmlRpc\Client;
 
-##$endpoint = 'http://lotsofyoga.com/mind-body-spirit/xmlrpc.php';
 $endpoint = 'http://cms.hostlite.co.uk/xmlrpc.php';
 $client   = new Client($endpoint);
 
@@ -12,9 +11,11 @@ $result   = $client->call('demo.sayHello');
 echo "XmlRpc response: ";
 print_r($result);
 echo "\n";
+
+
 echo "----\n";
 
-//$client = new Client('http://cms.hostlite.co.uk/xmlrpc.php');
+
 $params = array(
     1,
     'ingesta',
