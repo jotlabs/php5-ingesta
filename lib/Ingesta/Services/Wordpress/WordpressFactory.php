250aff9be95541414d7abf003d8068445ddd6484
Mike Davies <isofarro2@gmail.com>
2014-03-24 21:28:04 +0000

    add test mode to Wordpress Factory, to add in mock XMLRPC client.

--- lib/Ingesta/Services/Wordpress/WordpressFactory.php
+++ lib/Ingesta/Services/Wordpress/WordpressFactory.php
@@ -2,15 +2,33 @@
 namespace Ingesta\Services\Wordpress;
 
 use Ingesta\Clients\XmlRpc\ZendXmlRpcClient;
+use Ingesta\Clients\XmlRpc\MockXmlRpcClient;
 
 class WordpressFactory
 {
+    protected $isTestMode = false;
+
 
     public function getWordpressClient($apiEndpoint)
     {
-        $client    = $this->initXmlRpcClient($apiEndpoint);
+        $client = null;
+
+        if ($this->isTestMode) {
+            $client = $this->initMockClient($apiEndpoint);
+
+        } else {
+            $client    = $this->initXmlRpcClient($apiEndpoint);
+
+        }
+
         $wordpress = new Wordpress($client);
         return $wordpress;
+    }
+
+
+    public function setTestMode($isTestMode)
+    {
+        $this->isTestMode = $isTestMode;
     }
 
 
@@ -18,6 +36,13 @@ class WordpressFactory
     {
         // TODO: Refactor this into an XmlRpcFactory
         $client = new ZendXmlRpcClient($apiEndpoint);
+        return $client;
+    }
+
+
+    protected function initMockClient($apiEndpoint)
+    {
+        $client = new MockXmlRpcClient($apiEndpoint);
         return $client;
     }
 }
