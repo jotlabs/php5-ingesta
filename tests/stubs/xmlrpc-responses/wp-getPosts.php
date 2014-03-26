493a4e1220227b02818d7881461d491cd567c99b
Mike Davies <isofarro2@gmail.com>
2014-03-26 22:02:12 +0000

    mock response for wp.getPosts

---
+++ tests/stubs/xmlrpc-responses/wp-getPosts.php
@@ -0,0 +1,62 @@
+<?php
+
+$response = array(
+    array(
+        'post_id'           => 4,
+        'post_title'        => 'Hello World',
+        'post_date'         => '20140323T11:48:33',
+        'post_date_gmt'     => '20140323T11:48:33',
+        'post_modified'     => '20140323T11:48:33',
+        'post_modified_gmt' => '20140323T11:48:33',
+        'post_status'       => 'publish',
+        'post_type'         => 'post',
+        'post_name'         => 'hello-world',
+        'post_author'       => 1,
+        'post_password'     => '',
+        'post_excerpt'      => '',
+        'post_content'      => "This is a hello world post",
+        'post_parent'       => 0,
+        'post_mime_type'    => '',
+        'link' => 'http://example.com/greetings/hello-world/',
+        'guid' => 'http://example.com/?p=4',
+        'menu_order'     => 0,
+        'comment_status' => 'closed',
+        'ping_status'    => 'closed',
+        'sticky'         => '',
+        'post_thumbnail' => array (),
+        'post_format'    => 'standard',
+        'terms'          => array(
+            array(
+                'term_id'          => 3,
+                'name'             => 'Salutations',
+                'slug'             => 'salutations',
+                'term_group'       => 0,
+                'term_taxonomy_id' => 3,
+                'taxonomy'         => 'category',
+                'description'      => '',
+                'parent'           => 0,
+                'count'            => 1,
+                'filter'           => 'raw'
+            ),
+            array(
+                'term_id'          => 7,
+                'name'             => 'Interesting',
+                'slug'             => 'interesting',
+                'term_group'       => 0,
+                'term_taxonomy_id' => 7,
+                'taxonomy'         => 'post_tag',
+                'description'      => '',
+                'parent'           => 0,
+                'count'            => 1,
+                'filter'           => 'raw'
+            )
+        ),
+        'custom_fields'  => array(
+            array (
+                'id'    => 5,
+                'key'   => '_yoast_wpseo_metadesc',
+                'value' => 'Better metadescription title via plugin'
+            )
+        )
+    )
+);
