f3ee7fd98dc270a428c367164ef97de5882c8d02
Mike Davies <isofarro2@gmail.com>
2014-03-29 14:39:31 +0000

    implement wrapper to load a recipe and ready to start running it.

--- lib/Ingesta/Ingesta.php
+++ lib/Ingesta/Ingesta.php
@@ -4,17 +4,19 @@ namespace Ingesta;
 class Ingesta
 {
     const VERSION = '0.0.1';
+    const RECIPE_DIR = 'share/recipes';
 
     protected static $shortCmdOptions = '';
     protected static $longCmdOptions  = array(
-        'help'
+        'help',
+        'recipe::'
     );
 
 
     public static function run()
     {
         echo 'Ingesta v', self::VERSION, "\n";
-        $self = new Ingesta();
+        $self    = new Ingesta();
         $cmdArgs = $self->getCmdArgs();
         $self->execute($cmdArgs);
     }
@@ -23,6 +25,35 @@ class Ingesta
     public function execute($args)
     {
         print_r($args);
+        if (isset($args['recipe'])) {
+            $recipeName = $args['recipe'];
+            $recipe = $this->loadRecipe($recipeName);
+
+            if ($recipe) {
+                $this->executeRecipe($recipe);
+            }
+        }
+    }
+
+
+    protected function executeRecipe($recipe)
+    {
+        echo "Recipe to run: ";
+        print_r($recipe);
+
+    }
+
+
+    protected function loadRecipe($recipeName)
+    {
+        $recipe   = null;
+        $filePath = self::RECIPE_DIR . '/' . $recipeName . '.json';
+
+        if (is_file($filePath)) {
+            $recipe = json_decode(file_get_contents($filePath));
+        }
+
+        return $recipe;
     }
 
 
