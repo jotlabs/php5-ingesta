10e43cbb09606eb2b5167c7089dd0fde97012a13
Mike Davies <isofarro2@gmail.com>
2014-03-30 16:44:22 +0100

    Create a state object for ingesta recipes. Currently sets lastRun timestamp. Serialised as JSON.

--- lib/Ingesta/Ingesta.php
+++ lib/Ingesta/Ingesta.php
@@ -7,6 +7,7 @@ class Ingesta
 {
     const VERSION = '0.0.1';
     const RECIPE_DIR = 'share/recipes';
+    const STATE_DIR  = 'tmp/recipes';
 
     protected static $shortCmdOptions = '';
     protected static $longCmdOptions  = array(
@@ -32,17 +33,22 @@ class Ingesta
             $recipe = $this->loadRecipe($recipeName);
 
             if ($recipe) {
-                $this->executeRecipe($recipe);
+                $state = $this->loadState($recipeName);
+                $this->executeRecipe($recipe, $state);
+
+                $state->lastRun = date('c');
+                $this->saveState($recipeName, $state);
             }
         }
     }
 
 
-    protected function executeRecipe($recipe)
+    protected function executeRecipe($recipe, $state)
     {
         echo "Recipe to run: ";
         print_r($recipe);
-        $input = $this->getInput($recipe->input);
+        print_r($state);
+        $input     = $this->getInput($recipe->input);
 
     }
 
@@ -67,6 +73,42 @@ class Ingesta
         }
 
         return $recipe;
+    }
+
+
+    protected function loadState($recipeName)
+    {
+        $state    = null;
+        $filePath = self::STATE_DIR . '/' . $recipeName . '.state.json';
+
+        if (is_dir(dirname($filePath))) {
+            mkdir(dirname($filePath), 0777, true);
+        }
+
+        if (is_file($filePath)) {
+            $state = json_decode(file_get_contents($filePath));
+        } else {
+            $state = $this->initState($recipeName);
+        }
+
+        return $state;
+    }
+
+
+    protected function initState()
+    {
+        $state = (object) array ();
+
+        return $state;
+    }
+
+
+    protected function saveState($recipeName, $state)
+    {
+        $filePath = self::STATE_DIR . '/' . $recipeName . '.state.json';
+        $jsonDoc  = json_encode($state);
+        $response = file_put_contents($filePath, $jsonDoc);
+        return $response;
     }
 
 
