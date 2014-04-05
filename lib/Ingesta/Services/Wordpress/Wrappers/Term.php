5bf561c76c2aac96b9bb0b7e155677e3d6bb1684
Mike Davies <isofarro2@gmail.com>
2014-04-05 08:03:19 +0100

    Flesh out terms as a readable object.

--- lib/Ingesta/Services/Wordpress/Wrappers/Term.php
+++ lib/Ingesta/Services/Wordpress/Wrappers/Term.php
@@ -7,6 +7,42 @@ class Term
 
     public function __construct($term)
     {
-        $this->term = $term;
+        $this->term = (object) $term;
+    }
+
+
+    public function getTermId()
+    {
+        return $this->term->term_id;
+    }
+
+
+    public function getName()
+    {
+        return $this->term->name;
+    }
+
+
+    public function getSlug()
+    {
+        return $this->term->slug;
+    }
+
+
+    public function getDescription()
+    {
+        return $this->term->description;
+    }
+
+
+    public function getTaxonomy()
+    {
+        return $this->term->taxonomy;
+    }
+
+
+    public function getTotal()
+    {
+        return $this->term->count;
     }
 }
