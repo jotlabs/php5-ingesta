89c8d36c4f07b0f5b400cfdacf9c1faebb0569d7
Mike Davies <isofarro2@gmail.com>
2014-04-12 17:17:54 +0100

    Use WeblogUpdates class for dealing with blog pings.

--- etc/webapp/xmlrpc-blogping.php
+++ etc/webapp/xmlrpc-blogping.php
@@ -1,9 +1,18 @@
 <?php
 
+use Zend\XmlRpc\Server;
+use Ingesta\Servers\XmlRpc\WeblogUpdates;
+
 Flight::route('GET /ping/', function () {
     echo "XML-RPC Blogping server.";
 });
 
-Flight::route('POST /ping/', function () {
 
+Flight::route('POST /ping/', function () {
+    $server   = new  Zend\XmlRpc\Server();
+
+    $server->setClass(new WeblogUpdates(), 'weblogUpdates');
+
+    $response = $server->handle();
+    echo $response;
 });
