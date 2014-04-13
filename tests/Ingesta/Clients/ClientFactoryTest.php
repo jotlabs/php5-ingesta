2c9e6b2eed31f38e06859566a9675ed06e530219
Mike Davies <isofarro2@gmail.com>
2014-04-13 21:06:16 +0100

    test client factory creates proper HTTP clients

---
+++ tests/Ingesta/Clients/ClientFactoryTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace Ingesta\Clients;
+
+use PHPUnit_Framework_TestCase;
+
+class ClientFactoryTest extends PHPUnit_Framework_TestCase
+{
+    protected $factory;
+
+
+    public function setUp()
+    {
+        $this->factory = ClientFactory::getInstance();
+    }
+
+
+    public function testClassExists()
+    {
+        $this->assertTrue(class_exists('Ingesta\Clients\ClientFactory'));
+        $this->assertNotNull($this->factory);
+        $this->assertTrue(is_a($this->factory, 'Ingesta\Clients\ClientFactory'));
+    }
+
+
+    public function testGetHttpReturnsHttpClient()
+    {
+        $client = $this->factory->getHttpClient(null);
+        $this->assertNotNull($client);
+        $this->assertTrue(is_a($client, 'Ingesta\Clients\Http'));
+        $this->assertTrue(is_a($client, 'Ingesta\Clients\Http\HttpBase'));
+    }
+
+    public function testGetCachedHttpReturnsCacheHttpClient()
+    {
+        $client = $this->factory->getHttpClient(ClientFactory::CACHE_HTTP_CLIENT);
+        $this->assertNotNull($client);
+        $this->assertTrue(is_a($client, 'Ingesta\Clients\Http'));
+        $this->assertTrue(is_a($client, 'Ingesta\Clients\Http\CacheHttp'));
+    }
+}
