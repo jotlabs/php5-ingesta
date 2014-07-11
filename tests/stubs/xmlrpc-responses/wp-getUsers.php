99d903793b1b83938cf62007248a2e5cd13b76eb
Mike Davies <isofarro2@gmail.com>
2014-07-11 16:37:35 +0100

    stub xmlrpc response for wp.getUsers

---
+++ tests/stubs/xmlrpc-responses/wp-getUsers.php
@@ -0,0 +1,49 @@
+<?php
+
+$response = array(
+    array(
+        'user_id' => 2,
+        'username' => 'Alice',
+        'first_name' => 'Alice',
+        'last_name' => 'Writer',
+        'registered' => '20130718T20:04:15',
+        'bio' => '',
+        'email' => 'alice@example.com',
+        'nickname' => 'Alice',
+        'nicename' => 'alice',
+        'url' => '',
+        'display_name' => 'Alice',
+        'roles' => array('administrator')
+    ),
+
+    array(
+        'user_id' => 4,
+        'username' => 'Bob',
+        'first_name' => 'Bobby',
+        'last_name' => 'Drop Tables',
+        'registered' => '20140709T08:43:24',
+        'bio' => '',
+        'email' => 'bob@example.com',
+        'nickname' => 'Bob',
+        'nicename' => 'bob',
+        'url' => '',
+        'display_name' => 'Bobby Droptables',
+        'roles' => array('editor')
+    ),
+
+    array(
+        'user_id' => 1,
+        'username' => 'Carl',
+        'first_name' => '',
+        'last_name' => '',
+        'registered' => '20140314T12:19:39',
+        'bio' => '',
+        'email' => 'carl@example.com',
+        'nickname' => 'Carl',
+        'nicename' => 'carl',
+        'url' => '',
+        'display_name' => 'Carl',
+        'roles' => array('author')
+    ),
+
+);
