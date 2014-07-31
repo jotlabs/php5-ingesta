0ba6c8694e712c40168e8709c04ca409a9f089a0
Mike Davies <isofarro2@gmail.com>
2014-07-31 08:23:25 +0100

    Base class for services, offering html/json handling

---
+++ lib/Ingesta/Services/ServiceBase.php
@@ -0,0 +1,48 @@
+<?php
+namespace Ingesta\Services;
+
+use Ingesta\Inputs\InputGetter;
+
+abstract class ServiceBase implements InputGetter
+{
+    # Response codes
+    const RESPONSE_OK = 200;
+
+    protected $httpClient;
+    protected $urlTemplate;
+
+    abstract public function getInput($inputArgs);
+
+
+    public function __construct($httpClient, $urlTemplate = null)
+    {
+        $this->httpClient  = $httpClient;
+
+        if ($urlTemplate) {
+            $this->urlTemplate = $urlTemplate;
+        }
+    }
+
+
+
+    protected function getJson($url)
+    {
+        $response = $this->httpClient->get($url);
+        $json     = json_decode($response);
+        return $json;
+    }
+
+
+    protected function getHTML($url)
+    {
+        $response = $this->httpClient->get($url);
+        $html     = $response;
+        return $html;
+    }
+
+
+    protected function expandUrlTemplate($urlTemplate, $data)
+    {
+        return $this->urlTemplate->renderTemplate($urlTemplate, $data);
+    }
+}
