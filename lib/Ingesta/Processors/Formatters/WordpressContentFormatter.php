ba030a172cd0a093fde3a106d852cce296bd9005
Mike Davies <isofarro2@gmail.com>
2014-10-05 21:59:00 +0100

    Repair the primitive Wordpress content formatter to leave blocklevel elements and their closing tags alone when it comes to paragraph wrapping.

--- lib/Ingesta/Processors/Formatters/WordpressContentFormatter.php
+++ lib/Ingesta/Processors/Formatters/WordpressContentFormatter.php
@@ -9,7 +9,7 @@ class WordpressContentFormatter implements Processor
         'address', 'article', 'aside', 'audio', 'blockquote', 'canvas',
         'dd', 'div', 'dl', 'fieldset', 'figcaption', 'figure', 'footer',
         'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'hgroup',
-        'hr', 'noscript', 'ol', 'output', 'p', 'pre', 'section', 'table',
+        'hr', 'li', 'noscript', 'ol', 'output', 'p', 'pre', 'section', 'table',
         'tfoot', 'ul', 'video'
     );
 
@@ -30,18 +30,22 @@ class WordpressContentFormatter implements Processor
         $lines  = explode("\n", $content);
 
         foreach ($lines as $line) {
+            $line = trim($line);
+
             if ($line) {
                 if (preg_match("/^\[caption ([^\]]+)\](.*)\[\/caption\]$/", $line, $matches)) {
                     //print_r($matches);
                     $buffer[] = "<div class=\"media-caption\">{$matches[2]}</div>";
 
-                } elseif (preg_match("/^\<(\w+)\b/", $line, $match) && in_array($match[1], self::$blockEl)) {
+                } elseif (preg_match("/^\<\/?(\w+)\b/", $line, $match) && in_array($match[1], self::$blockEl)) {
                     // Do not wrap block level elements
                     $buffer[] = $line;
 
-                } else {
+                } elseif (preg_match('/\S+/', $line)) {
                     $buffer[] = "<p>{$line}</p>";
 
+                } else {
+                    $buffer[] = $line;
                 }
             }
         }
